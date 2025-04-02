a3acaefb37485e164042fb8992d36c5f78e2a4af
pampukhaleo <lgpampukha@gmail.com>
2025-04-02 13:24:59 +0300

    navbar logo fixed

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -5,7 +5,7 @@ export default function Logo() {
   return (
     <Link to="/" className="flex items-center flex-shrink-0">
       <img 
-        src="/lovable-uploads/logo.jpg" 
+        src="/lovable-uploads/1373ca5d-af25-42c9-bb89-5c42a7e6fa2e.png"
         alt="European Arbitration Chamber Logo" 
         className="h-12 w-auto" 
         style={{ maxWidth: '100%' }}

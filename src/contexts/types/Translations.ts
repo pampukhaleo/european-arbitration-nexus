fd76feaba3629a043ae7fbd80f7e3fa93f9986d8
pampukhaleo <lgpampukha@gmail.com>
2025-04-05 00:45:16 +0300

    Translations.ts edit

--- src/contexts/types/Translations.ts
+++ src/contexts/types/Translations.ts
@@ -1,3 +1,7 @@
 export type Translations = {
-  [key: string]: string | string[] | Translations;
+  [key: string]:
+    | string
+    | string[]
+    | Translations
+    | { [key: string]: string }[]; // for arrays of title-description or similar objects
 };
